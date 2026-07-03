32f7b58a53251db943613062e4ad6486bf7c8cab
lifegmr12-afk <lifegmr12@gmail.com>
2026-07-03 13:52:59 +0545

    Updated build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c51e36e29ffc7c40"; }
+/******/ 		__webpack_require__.h = function() { return "7f5f09e20f41728c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
